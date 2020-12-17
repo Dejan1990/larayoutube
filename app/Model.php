442c16257b5cbe31a5737c92f603133e335f75c0
dejan1990 <dejan.simo@hotmail.com>
2020-12-17 13:40:02 +0100

    Implementing uuid, create our Model(BaseModel)

---
+++ app/Model.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+class Model extends BaseModel
+{
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            //$model->id = Str::uuid(); // Moze ovako, ali nije najbolje
+
+            $model->{$model->getKeyName()} = (string) Str::uuid(); // return primaryKey // if overated, still work
+        });
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -9,6 +10,19 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public $incrementing = false; //posto nasledjujemo Authenticatable, ne mozemo i nas Model(BaseModel), pa ovo ostaje ovde
+
+    protected static function boot() //posto nasledjujemo Authenticatable, ne mozemo i nas Model(BaseModel), pa ovo ostaje ovde
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            //$model->id = Str::uuid(); // Moze ovako, ali nije najbolje
+
+            $model->{$model->getKeyName()} = (string) Str::uuid(); // return primaryKey // if overated, still work
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
