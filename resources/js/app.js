67a2d96c8c15ac881f441f65628b0182b94f836e
dejan1990 <dejan.simo@hotmail.com>
2020-12-19 11:39:38 +0100

    Channel uploads component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 require('./components/subscribe-button').default
+require('./components/channel-uploads').default
 
 const app = new Vue({
     el: '#app',
