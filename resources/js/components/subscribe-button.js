33922108ac68d5fc0aa5efdf931a922462e73456
dejan1990 <dejan.simo@hotmail.com>
2020-12-19 08:35:51 +0100

    Subscribe and Unsubscribe

--- resources/js/components/subscribe-button.js
+++ resources/js/components/subscribe-button.js
@@ -1,3 +1,4 @@
+import Axios from 'axios';
 import numeral from 'numeral';
 
 Vue.component('subscribe-button', {
@@ -19,7 +20,7 @@ Vue.component('subscribe-button', {
         subscribed() {
             if (! __auth() || this.channel.user_id === __auth().id) return false
 
-            return !!this.subscriptions.find(subscription => subscription.user_id === __auth().id)
+            return !!this.subscription
         },
 
         owner() {
@@ -28,15 +29,31 @@ Vue.component('subscribe-button', {
             return false
         },
 
+        subscription() {
+            if(! __auth()) return null
+
+            return this.subscriptions.find(subscription => subscription.user_id === __auth().id)
+        },
+
         count() {
-            return numeral(this.subscriptions.length).format('0a')
+            return numeral(this.subscriptions.length).format('0a.0')
         }
     },
 
     methods: {
         toggleSubscription() {
             if (!__auth()) {
-                alert('Please log in to subscribe');
+                return alert('Please log in to subscribe');
+            }
+
+            if(this.owner) {
+                return alert('You cannot subscribe to your channel');
+            }
+
+            if(this.subscribed) {
+                axios.delete(`/channels/${this.channel.id}/subscriptions/${this.subscription.id}`)
+            } else {
+                axios.post(`/channels/${this.channel.id}/subscriptions`)
             }
         }
     }
