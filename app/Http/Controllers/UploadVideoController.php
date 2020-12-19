0d667a276afe134f4d87e0aa5e67c57e00d6dc43
dejan1990 <dejan.simo@hotmail.com>
2020-12-19 14:12:29 +0100

    Ajax video uploads

--- app/Http/Controllers/UploadVideoController.php
+++ app/Http/Controllers/UploadVideoController.php
@@ -9,6 +9,16 @@ class UploadVideoController extends Controller
 {
     public function index(Channel $channel)
     {
-        return view('channels.upload', compact('channel'));
+        return view('channels.upload', [
+            'channel' => $channel
+        ]);
+    }
+
+    public function store(Channel $channel)
+    {
+        return $channel->videos()->create([
+            'title' => request()->title,
+            'path' => request()->video->store("channels/{$channel->id}")
+        ]);
     }
 }
--- resources/js/components/channel-uploads.js
+++ resources/js/components/channel-uploads.js
@@ -1,15 +1,32 @@
 Vue.component('channel-uploads', {
+    props: {
+        channel: {
+            type: Object,
+            required: true,
+            default: () => ({})
+        }
+    },
     data() {
         return {
-            selected: false
+            selected: false,
+            videos: []
         }
     },
 
     methods: {
         upload() {
             this.selected = true
-            const videos = this.$refs.videos.files
-            console.log(videos)
+            this.videos = Array.from(this.$refs.videos.files) // Array.from konvertuje u array
+
+            const uploaders = this.videos.map(video => {    //posto je array mozemo da koristimo map -> mapoverate // map ocekuje da vratimo rezultat i onda map menja pozicije u novom array-u
+
+                const form = new FormData()
+
+                form.append('video', video)
+                form.append('title', video.name)
+
+                return axios.post(`/channels/${this.channel.id}/videos`, form)
+            }) 
         }
     },
 })
